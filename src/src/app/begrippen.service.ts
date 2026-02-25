3890759812dbba0499069173c8675a5c7853e8fd
mike643 <157297368+mike643@users.noreply.github.com>
2026-02-25 16:02:00 +0000

    biologie added

--- src/src/app/begrippen.service.ts
+++ src/src/app/begrippen.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
@@ -11,13 +11,43 @@ export interface BegripData {
 export class BegrippenService {
   private data: Record<string, any> | null = null;
   private keys: string[] = [];
+  currentFilePath = signal<string | null>(null);
 
   constructor(private http: HttpClient) {}
 
+  async loadFile(filePath: string) {
+    this.currentFilePath.set(filePath);
+    this.data = null;
+    this.keys = [];
+    await this.ensureLoaded();
+  }
+
   private async ensureLoaded() {
     if (this.data) return;
-    const url = 'content/ak/h4/begrippen.json';
-    this.data = await firstValueFrom(this.http.get<Record<string, any>>(url));
+    const filePath = this.currentFilePath() || 'content/ak/h4/begrippen.json';
+    const url = `content/${filePath}`;
+    
+    const rawData = await firstValueFrom(this.http.get<any>(url));
+    
+    // Check if the data has a "begrippen" array (new format)
+    if (Array.isArray(rawData.begrippen)) {
+      // Transform from array format to key-value format
+      this.data = {};
+      rawData.begrippen.forEach((item: any) => {
+        const key = item.begrip;
+        this.data![key] = {
+          boek_omschrijving: item.definitie,
+          eenvoudige_omschrijving_1: item.uitleg_voor_kind?.[0] || '',
+          eenvoudige_omschrijving_2: item.uitleg_voor_kind?.[1] || '',
+          ezelsbrug: item.ezelsbrug || '',
+          mc_vragen: item.vragen || []
+        };
+      });
+    } else {
+      // Old format: direct key-value map
+      this.data = rawData;
+    }
+    
     this.keys = Object.keys(this.data || {});
   }
 
--- src/src/app/start.component.ts
+++ src/src/app/start.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 import { BegrippenService } from './begrippen.service';
 
 @Component({
@@ -10,24 +12,61 @@ import { BegrippenService } from './begrippen.service';
   templateUrl: './start.component.html'
 })
 export class StartComponent {
-  keys: string[] = [];
+  content = signal<any>(null);
+  selectedVak = signal<string | null>(null);
+  selectedThema = signal<string | null>(null);
 
-  constructor(private svc: BegrippenService, private router: Router) {
-    this.load();
+  
+  constructor(private http: HttpClient, private svc: BegrippenService, private router: Router) {
+    this.loadContent();
   }
 
-  async load() {
-    const count = await this.svc.count();
-    // build keys by fetching successive entries
-    const tmp: string[] = [];
-    for (let i = 0; i < count; i++) {
-      const item = await this.svc.getByIndex(i);
-      if (item) tmp.push(item.key);
-    }
-    this.keys = tmp;
+  allVakken = computed(() => {
+    const c = this.content();
+    if (!c || !c.vakken) return [];
+    return Object.entries(c.vakken).map(([id, data]: any) => ({
+      id,
+      ...data
+    }));
+  });
+
+  themaForVak = computed(() => {
+    const vak = this.selectedVak();
+    const c = this.content();
+    if (!vak || !c || !c.vakken || !c.vakken[vak]) return [];
+    const themas = c.vakken[vak].themas || {};
+    return Object.entries(themas).map(([id, data]: any) => ({
+      id,
+      ...data
+    }));
+  });
+
+  hoofdstukkenvoor = computed(() => {
+    const vak = this.selectedVak();
+    const thema = this.selectedThema();
+    const c = this.content();
+    if (!vak || !thema || !c || !c.vakken || !c.vakken[vak]) return [];
+    const themaObj = c.vakken[vak].themas?.[thema];
+    return themaObj?.hoofdstukken || [];
+  });
+
+  async loadContent() {
+    const data = await firstValueFrom(this.http.get<any>('content/content.json'));
+    this.content.set(data);
   }
 
-  startLeren() {
+  selectVak(vakId: string) {
+    this.selectedVak.set(vakId);
+    this.selectedThema.set(null);
+  }
+
+  selectThema(themaId: string) {
+    this.selectedThema.set(themaId);
+  }
+
+  async selectHoofdstuk(hoofdstuk: any) {
+    const bestandspad = hoofdstuk.bestand;
+    await this.svc.loadFile(bestandspad);
     this.router.navigate(['/begrip', 0]);
   }
 
